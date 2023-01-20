f4da7b2b513f109cef36c14268ecc9b6d1180bd0
jpstudy1988 <69568139+jpstudy1988@users.noreply.github.com>
2023-01-20 14:35:10 +0530

    Initial commit

---
+++ src/main/java/com/jp/practice/romanconversionapi/RomanconversionapiApplication.java
@@ -0,0 +1,14 @@
+package com.jp.practice.romanconversionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomanconversionapiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RomanconversionapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jp/practice/romanconversionapi/RomanconversionapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.practice.romanconversionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomanconversionapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
